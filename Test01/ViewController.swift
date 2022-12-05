9df13c264133b362ff835858a4d6c38097710146
VyacheslavRostovtsev <119050338+VyacheslavRostovtsev@users.noreply.github.com>
2022-12-05 17:32:47 +0300

    First try

--- Test01/ViewController.swift
+++ Test01/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    private var count: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func countButton(_ sender: UIButton) {
+        count += 1
+        countLabel.text = "Значение счетчика: \(count)"
+    }
+    
 }
 
