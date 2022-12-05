1457a4426369aa1db0483e5ee3e7cd7da416aee2
VyacheslavRostovtsev <119050338+VyacheslavRostovtsev@users.noreply.github.com>
2022-12-05 17:08:18 +0300

    Initial Commit

---
+++ Test01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test01
+//
+//  Created by Вячеслав Ростовцев on 05.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
